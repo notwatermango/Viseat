ef64a39a7b831f03e5624137408ea85e0147e4e9
danzelmba15 <notwatermango@gmail.com>
2024-04-02 12:03:47 +0700

    Initial Commit

---
+++ Viseat/ViseatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViseatApp.swift
+//  Viseat
+//
+//  Created by mg0 on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ViseatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
